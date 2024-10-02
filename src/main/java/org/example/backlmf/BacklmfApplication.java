e079bf17a9a3deb8acea303958f00902ad25e5a4
世间微斯人 <93172457+HT3301601278@users.noreply.github.com>
2024-10-02 12:18:55 +0800

    完善

---
+++ src/main/java/org/example/backlmf/BacklmfApplication.java
@@ -0,0 +1,13 @@
+package org.example.backlmf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacklmfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BacklmfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/backlmf/BacklmfApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backlmf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacklmfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
